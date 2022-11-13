0b302b00c2b846b4b916ccea692575e139708bf9
Jeet Dholakia <onemediasoft@gmail.com>
2022-11-13 14:52:46 +0530

    Fixes crash in index.js

--- src/index.js
+++ src/index.js
@@ -14,13 +14,6 @@ app.get('/notes', async (req, res) => {
     } catch(err) {
         res.status(500).send(err)
     }
-    
-    fs.readFile(__dirname + '/' + "notes.json", 'utf-8', (err, data) => {
-        if(err) {
-            return console.log(err)
-        }
-        res.send(data)
-    })
 })
 
 app.post('/notes', async (req, res) => {
@@ -37,14 +30,15 @@ app.post('/notes', async (req, res) => {
 app.patch('/notes/:id', async (req, res) => {
     try {
         const note = await Note.findById(req.params.id)
+        console.log(note)
         if(!note) {
             res.status(404).send(err)
         }
         note.note = req.body.note
         await note.save()
-        req.status(200).send(note)
+        res.status(200).send(note)
     } catch (err) {
-        res.status(404).send(err)
+        res.status(500).send(err)
     }
 })
 
