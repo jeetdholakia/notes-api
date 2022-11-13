f9f212cf149155d853417369545a877af2e8d30d
Jeet Dholakia <onemediasoft@gmail.com>
2022-11-13 07:01:03 +0530

    Added PATCH request

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const Note = require('./models/note')
 const app = express()
 app.use(express.json())
 
+
 app.get('/notes', async (req, res) => {
     try {
         const notes = await Note.find({})
@@ -30,6 +31,20 @@ app.post('/notes', async (req, res) => {
         res.status(201).send(note)
     } catch(err) {
         res.status(500).send(err)
+    }
+})
+
+app.patch('/notes/:id', async (req, res) => {
+    try {
+        const note = await Note.findById(req.params.id)
+        if(!note) {
+            res.status(404).send(err)
+        }
+        note.note = req.body.note
+        await note.save()
+        req.status(200).send(note)
+    } catch (err) {
+        res.status(404).send(err)
     }
 })
 
