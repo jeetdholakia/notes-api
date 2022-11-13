cc79690ec2b287bca4a47076c0b574f5bf24a5a7
Jeet Dholakia <onemediasoft@gmail.com>
2022-11-13 06:54:16 +0530

    Revert "GET notes"

--- src/index.js
+++ src/index.js
@@ -1,36 +1,16 @@
 const express = require("express")
 const fs = require("fs")
-require('./db/mongoose')
-const Note = require('./models/note')
+require('.db/mongoose.js')
 
 const app = express()
-app.use(express.json())
 
-app.get('/notes', async (req, res) => {
-    try {
-        const notes = await Note.find({})
-        res.status(200).send(notes)
-    } catch(err) {
-        res.status(500).send(err)
-    }
-    
+app.get('/notes', (req, res) => {
     fs.readFile(__dirname + '/' + "notes.json", 'utf-8', (err, data) => {
         if(err) {
             return console.log(err)
         }
         res.send(data)
     })
-})
-
-app.post('/notes', async (req, res) => {
-    const note = new Note(req.body)
-
-    try {
-        await note.save()
-        res.status(201).send(note)
-    } catch(err) {
-        res.status(500).send(err)
-    }
 })
 
 app.listen(3000, () => {
--- src/models/note.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const schema = new mongoose.Schema({ note: 'string'});
-const Note = mongoose.model('Note', schema)
-
-module.exports = Note
